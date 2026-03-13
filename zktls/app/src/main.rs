cd89f2c5fd80e91a543ce0cae99388fd2897af60
ujnss <unlinunlin@163.com>
2026-03-13 09:33:26 +0000

    add attestation conditions check

--- zktls/app/src/main.rs
+++ zktls/app/src/main.rs
@@ -4,10 +4,35 @@ use anyhow::Result;
 use pico_sdk::io::{commit, read_as};
 use zktls_att_verification::attestation_data::verify_attestation_data;
 
+// Different data sources use different configuration parameters.
+// Refer to `zktls/data/xxx_config.json` and replace the content of ATTESTATION_CONFIG.
 const ATTESTATION_CONFIG: &str = r#"{
   "attestor_addr": "0xe02bd7a6c8aa401189aebb5bad755c2610940a73",
   "url": [
-    "https://github.com/_global-navigation/payloads.json"
+    "https://github.com/_global-navigation/payloads.json",
+    "https://github.com"
+  ],
+  "conditions": [
+    {
+      "op": "REVEAL_SALTTED_HASH",
+      "field": "{\"type\":\"FIELD_ARITHMETIC\",\"op\":\"SHA256_WITH_SALT\",\"field\":\"$.owner.avatarUrl\"}",
+      "reveal_id": "github_id"
+    },
+    {
+      "op": "REVEAL_SALTTED_HASH",
+      "field": "{\"type\":\"FIELD_ARITHMETIC\",\"op\":\"SHA256_WITH_SALT\",\"field\":\"//h2[@id=\\\"js-contribution-activity-description\\\"]\"}",
+      "reveal_id": "contribution"
+    },
+    {
+      "op": "REVEAL_SALTTED_HASH",
+      "field": "{\"type\":\"FIELD_ARITHMETIC\",\"op\":\"SHA256_WITH_SALT\",\"field\":\"//div[@id=\\\"year-list-container\\\"]//ul[@class=\\\"filter-list small\\\"]/li[*]/a\"}",
+      "reveal_id": "years"
+    },
+    {
+      "op": "REVEAL_SALTTED_HASH",
+      "field": "{\"type\":\"FIELD_ARITHMETIC\",\"op\":\"SHA256_WITH_SALT\",\"field\":\"//a[@id=\\\"year-link-2026\\\"]/@data-hydro-click\"}",
+      "reveal_id": "github_id_in_html"
+    }
   ]
 }"#;
 
